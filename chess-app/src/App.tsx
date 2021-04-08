8eff0d17d6476b08e1977c46ca28940e50c18a65
Fabricio Xavier <fabricioxavierchagas@gmail.com>
2021-04-08 14:10:14 -0300

    Creating chess board

---
+++ chess-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Chessboard from './components/Chessboard';
+import './App.css'
+
+function App() {
+  return (
+    <div id="app">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
