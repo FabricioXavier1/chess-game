587d55066346481e30add3d54cb2919ded732e95
Fabricio Xavier <fabricioxavierchagas@gmail.com>
2021-04-09 17:44:23 -0300

    initial position chess

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -12,14 +12,30 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
-for(let i = 0; i < 8; i++){
+for(let p = 0; p < 2; p++){
 
-    pieces.push({image: "assets/images/pawn_b.png", x:i, y: 6})
+    const type = p === 0 ? "b" : "w";
+    const y = p === 0 ? 7 : 0;
+
+    pieces.push({image: `assets/images/rook_${type}.png`, x: 0, y})
+    pieces.push({image: `assets/images/rook_${type}.png`, x: 7, y})
+    pieces.push({image: `assets/images/knight_${type}.png`, x: 1, y})
+    pieces.push({image: `assets/images/knight_${type}.png`, x: 6, y})
+    pieces.push({image: `assets/images/bishop_${type}.png`, x: 2, y})
+    pieces.push({image: `assets/images/bishop_${type}.png`, x: 5, y})
+    pieces.push({image: `assets/images/queen_${type}.png`, x: 3, y})
+    pieces.push({image: `assets/images/king_${type}.png`, x: 4, y})
+
 }
 
 for(let i = 0; i < 8; i++){
 
     pieces.push({image: "assets/images/pawn_w.png", x:i, y: 1})
+}
+
+for(let i = 0; i < 8; i++){
+
+    pieces.push({image: "assets/images/pawn_b.png", x:i, y: 6})
 }
 
 export default function Chessboard() {
@@ -36,7 +52,7 @@ export default function Chessboard() {
                 }
             })
 
-            board.push(<Tile image={image} number={i + j + 2} />)
+            board.push(<Tile key={`${j}, ${i}`} image={image} number={i + j + 2} />)
 
         }
     }
